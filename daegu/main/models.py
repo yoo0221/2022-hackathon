77ce8e720202e3ac1b25d743a353eaa4da3722cb
yoo020221 <niceyoo0221@knu.ac.kr>
2022-09-23 16:15:59 +0900

    models 주석 처리

--- daegu/main/models.py
+++ daegu/main/models.py
@@ -1,31 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
-class User(AbstractUser):
-    saved_place = models.ForeignKey()
-    course = models.ForeignKey()
 
-class Place(models.Model):
-    name = models.CharField()
-    latitude = models.FloatField()
-    longtitude = models.FloatField()
-    description = models.TextField()
-    category = models.CharField()
-    like = models.IntegerField()
-    photo = models.ForeignKey()
+# class ScrappedPlace(models.Model):
+#     name = models.CharField()
+#     lat = models.FloatField()
+#     lng = models.FloatField()
+#     description = models.TextField()
+#     category = models.CharField()
+#     like = models.IntegerField()
 
-class Photo(models.Model):
-    src = models.ImageField()
+# class Photo(models.Model):
+#     src = models.ImageField()
+#     scrappedplace = models.ForeignKey(ScrappedPlace, on_delete=False)
 
-class Course(models.Model):
-    name = models.CharField()
-    place = models.ForeignKey()
-    copyed_count = models.IntegerField()
+# class Course(models.Model):
+#     name = models.CharField()
+#     copyed_count = models.IntegerField()
 
-class AdminPlace(models.Model):
-    hash_category = models.CharField()
-    writer = models.CharField()
-    description = models.CharField()
-    like = models.IntegerField()
-    category = models.CharField()
-    comment = models.ForeignKey()
+# class Place(models.Model):
+#     name = models.CharField()
+#     latitude = models.FloatField()
+#     longtitude = models.FloatField()
+#     description = models.TextField()
+#     category = models.CharField()
+#     like = models.IntegerField()
+
+# class Place(models.Model):
+#     name = models.CharField()
+#     latitude = models.FloatField()
+#     longtitude = models.FloatField()
+#     description = models.TextField()
+#     category = models.CharField()
+#     like = models.IntegerField()
+#     photo = models.ForeignKey()
+
+# class Photo(models.Model):
+#     src = models.ImageField()
+
+# class Course(models.Model):
+#     name = models.CharField()
+#     place = models.ForeignKey()
+#     copyed_count = models.IntegerField()
+
+# class AdminPlace(models.Model):
+#     hash_category = models.CharField()
+#     writer = models.CharField()
+#     description = models.CharField()
+#     like = models.IntegerField()
+#     category = models.CharField()
+#     comment = models.ForeignKey()
